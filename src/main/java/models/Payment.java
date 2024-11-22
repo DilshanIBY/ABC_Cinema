90b76353f15e7fc33322d20c30a1f9bc95fbdb72
G.Kishalan <G.Kishalan@Kishalan>
2024-11-22 20:26:52 +0530

    first commit

--- src/main/java/models/Payment.java
+++ src/main/java/models/Payment.java
@@ -1,1 +1,67 @@
 // Placeholder content for Payment.java
+public class Payment {
+    private int paymentId;
+    private int reservationId;
+    private double amount;
+    private String paymentMethod;
+    private boolean isSuccessful;
+
+    public Payment(int paymentId, int reservationId, double amount, String paymentMethod, boolean isSuccessful) {
+        this.paymentId = paymentId;
+        this.reservationId = reservationId;
+        this.amount = amount;
+        this.paymentMethod = paymentMethod;
+        this.isSuccessful = isSuccessful;
+    }
+
+    public int getPaymentId() {
+        return paymentId;
+    }
+
+    public void setPaymentId(int paymentId) {
+        this.paymentId = paymentId;
+    }
+
+    public int getReservationId() {
+        return reservationId;
+    }
+
+    public void setReservationId(int reservationId) {
+        this.reservationId = reservationId;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public boolean isSuccessful() {
+        return isSuccessful;
+    }
+
+    public void setSuccessful(boolean successful) {
+        isSuccessful = successful;
+    }
+
+    @Override
+    public String toString() {
+        return "Payment{" +
+                "paymentId=" + paymentId +
+                ", reservationId=" + reservationId +
+                ", amount=" + amount +
+                ", paymentMethod='" + paymentMethod + '\'' +
+                ", isSuccessful=" + isSuccessful +
+                '}';
+    }
+}
